034566757eb70edcd78524b3db9dc5e8f262d163
Islam Sanatbek <qwant684@gmail.com>
2025-09-02 21:49:14 +0500

    Dockerize

--- app/db.py
+++ app/db.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os 
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost/workouts")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://Qwant:12345@localhost/workouts")
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 new_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
